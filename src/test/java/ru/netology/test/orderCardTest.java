d767ba923fb26e4da8c70ce8106f52ec975e9bcf
zenitfan88 <nikanenkov@rambler.ru>
2022-10-31 19:13:35 +0400

    add validation

--- src/test/java/ru/netology/test/orderCardTest.java
+++ src/test/java/ru/netology/test/orderCardTest.java
@@ -49,4 +49,66 @@ public class orderCardTest {
                 text.trim());
     }
 
+    @Test
+    void testName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("+79278228262");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79278228262");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_theme_alfa-on-white")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.",
+                text.trim());
+    }
+
+    @Test
+    void testPhone() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иван");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+7927822826");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_theme_alfa-on-white")).click();
+        String text = driver.findElement(By.xpath("//span[text()='Мобильный телефон']/following-sibling:" +
+                ":span[contains(@class, 'input__sub')]")).getText();
+
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.",
+                text.trim());
+    }
+
+    @Test
+    void testNameNull() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+7927822826");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_theme_alfa-on-white")).click();
+        String text = driver.findElement(By.xpath("//span[text()='Фамилия и имя']/following-sibling:" +
+                ":span[contains(@class, 'input__sub')]")).getText();
+        assertEquals("Поле обязательно для заполнения",
+                text.trim());
+    }
+
+    @Test
+    void testPhoneNull() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иван");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_theme_alfa-on-white")).click();
+        String text = driver.findElement(By.xpath("//span[text()='Мобильный телефон']/following-sibling" +
+                "::span[contains(@class, 'input__sub')]")).getText();
+        assertEquals("Поле обязательно для заполнения",
+                text.trim());
+    }
+
+    @Test
+    void testCheckbox() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Иван");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79278228262");
+        driver.findElement(By.className("button_theme_alfa-on-white")).click();
+        String text = driver.findElement(By.className("checkbox__text")).getCssValue("color");
+        assertEquals("rgba(255, 92, 92, 1)", text.trim());
+    }
 }
